0d30e0bcedf31c1dcf6655e1172f63ec2eb0d71a
Yogesh Korke <yogikorke91@gmail.com>
2024-05-03 18:18:53 +0530

    Isolated components and their styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
-import reactImage from "./assets/react-core-concepts.png";
-import componentImage from "./assets/components.png";
-
-const reactDescription = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescription[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImage} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcepts(props) {
-  return (
-    <li>
-      <img src={props.image} alt="..." />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header";
+import CoreConcepts from "./components/CoreConcepts/CoreConcept";
 
 function App() {
   return (
@@ -40,10 +10,10 @@ function App() {
         <section id="core-concepts">
           <h2> Core Concepts </h2>
           <ul>
-            <CoreConcepts title="Components" description="The core UI building block." image={componentImage}></CoreConcepts>
-            <CoreConcepts></CoreConcepts>
-            <CoreConcepts></CoreConcepts>
-            <CoreConcepts></CoreConcepts>
+            <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}></CoreConcepts>
+            <CoreConcepts title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}></CoreConcepts>
+            <CoreConcepts title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}></CoreConcepts>
+            <CoreConcepts title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}></CoreConcepts>
           </ul>
         </section>
       </main>
